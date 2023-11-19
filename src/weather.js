c8f30967abfc93f99c7f8acabf7160f6b1835413
Pavel <magma4810@gmail.com>
2023-11-19 18:24:18 +0300

    show weather from button

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,6 @@
 const list = [];
+const city = [];
+const key = "ab4639f5754271e773ed6d3ffd73f327";
 export const showWeather = async (selector) => {
   document.body.append(selector);
   const input = document.createElement("input");
@@ -11,25 +13,48 @@ export const showWeather = async (selector) => {
   const showWeatherHTML = document.createElement("b");
   selector.append(listHTML);
   selector.append(showWeatherHTML);
-  const key = "ab4639f5754271e773ed6d3ffd73f327";
   const buttonContainer = document.createElement("div");
   selector.append(buttonContainer);
   button.addEventListener("click", async () => {
     const value = input.value;
-    const reject = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${value}&appid=${key}`,
-    );
-    const weather = await reject.json();
+    const weather = await getAndShowWeather(value, showWeatherHTML);
     input.value = "";
     let temp = weather.main.temp;
     let nameCity = weather.name;
-    list.push({ temp, nameCity });
-    showWeatherHTML.innerHTML = `${weather.main.temp}`;
-    await addButton(buttonContainer);
+    let icon = weather.weather[0].icon;
+    list.unshift({ temp, nameCity, icon });
+    city.push(nameCity);
+    // console.log(city,city.includes(value))
+    // if(!city.includes(value)){
+    //     addButton(buttonContainer,showWeatherHTML);
+    // }
+    addButton(buttonContainer, showWeatherHTML);
   });
 };
-export const addButton = (selector) => {
-  selector.innerHTML = `<ul>${list
-    .map((el) => `<li><button>${el.nameCity}</button></li>`)
-    .join("")}</ul>`;
+export const getAndShowWeather = async (city, selector) => {
+  const reject = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=
+        ${city}&appid=${key}`,
+  );
+  const weather = await reject.json();
+  selector.innerHTML = `${`Погода в ${weather.name} ${
+    weather.main.temp
+  }˚<img src = 
+    ${`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}>`}`;
+  return weather;
+};
+export const addButton = (selector, showWeatherHTML) => {
+  const button = document.createElement("button");
+  button.innerHTML = list[0].nameCity;
+  selector.append(button);
+  showWeatherCityButton(button, showWeatherHTML);
+};
+export const showWeatherCityButton = (selector, showWeatherHTML) => {
+  selector.addEventListener("click", async () => {
+    for (let i = 0; i < list.length; i++) {
+      if (selector.innerHTML === list[i].nameCity) {
+        await getAndShowWeather(list[i].nameCity, showWeatherHTML);
+      }
+    }
+  });
 };
